d7320854b1c91ba171391a3598cd70cbb0138532
PENGUINLIONG <admin@penguinliong.moe>
2022-10-01 16:04:29 +0800

    Restructured project

--- scripts/Update-ExampleLogs.ps1
+++ scripts/Update-ExampleLogs.ps1
@@ -1,3 +1,5 @@
+Push-Location ./spirq
 foreach ($example in Get-ChildItem -Path "$PWD/examples") {
     & cargo run --example "$($example.Name)" > "examples/$($example.Name)/main.log" 
 }
+Pop-Location
