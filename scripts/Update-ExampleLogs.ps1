e6ec775be51f32bee35fb451fa66b1226e4e0166
PENGUINLIONG <admin@penguinliong.moe>
2023-05-26 18:41:27 +0800

    Update test logs

--- scripts/Update-ExampleLogs.ps1
+++ scripts/Update-ExampleLogs.ps1
@@ -1,3 +1,9 @@
 foreach ($example in Get-ChildItem -Path "spirq/examples") {
     & cargo run --example "$($example.Name)" > "spirq/examples/$($example.Name)/main.log" 
 }
+
+foreach ($shader in Get-ChildItem -Path "assets") {
+    if ($shader.Name -notlike "*.json") {
+        & cargo run -p shader-reflect "$shader" --reference-all-resources > "assets/$($shader.Name).json" 
+    }
+}
